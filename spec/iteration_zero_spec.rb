b1787021b2dccd1c4aac9f14dbefab9fc97c351f
Adam Hundley <adamhundley@gmail.com>
2016-01-04 17:16:54 -0700

    adding iteration zero tests

--- spec/iteration_zero_spec.rb
+++ spec/iteration_zero_spec.rb
@@ -7,16 +7,37 @@ require 'hurley'
 
 class IterartionZeroTest < Minitest::Test
 
-  attr_reader :client, :iterator
+  attr_reader :response
 
   def setup
-    @client = Hurley::Client.new("http://127.0.0.1:9292")
-    @iterator = 0
+    @response = Hurley.get("http://127.0.0.1:9292")
   end
 
   def test_response_success
-    response = Hurley.get("http://127.0.0.1:9292")
     assert response.success?
   end
+
+  def test_for_a_body
+    body_split = response.body.split
+    assert_equal "Hello,", body_split[0]
+  end
+
+  def test_for_the_url
+    url = response.request.url.to_s
+    assert_equal "http://127.0.0.1:9292", url
+  end
+
+  def test_for_the_verb
+    verb = response.request.verb
+    assert_equal :get, verb
+  end
+
+  def test_the_server_increments_each_request
+    first_time = response.body.split[2].split("")[1..2].join.to_i
+    second_response = Hurley.get("http://127.0.0.1:9292")
+    second_time = second_response.body.split[2].split("")[1..2].join.to_i
+    assert_equal second_time, first_time + 1
+  end
+
 
 end
