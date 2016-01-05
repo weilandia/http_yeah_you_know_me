d60a83d72ccca745c21b233ed029547ef56d960a
Nick Weiland <nickweiland@gmail.com>
2016-01-05 13:05:59 -0700

    Refactor into classes, func @ iter 1

--- lib/request.rb
+++ lib/request.rb
@@ -15,20 +15,30 @@ class Request
       request_lines << line.chomp
     end
 
-    verb = request_lines[0].split[0]
-    path = request_lines[0].split[1]
-    protocol = request_lines[0].split[2]
-    host = request_lines[1].split(":")[1].strip
-    port = request_lines[1].split(":").last
-    accept = request_lines[4].split(":")[1].strip
+    verb_path_protocol = request_lines.shift
+
+    verb = verb_path_protocol.split[0]
+    path = verb_path_protocol.split[1]
+    protocol = verb_path_protocol.split[2]
+
+
+    request_lines_hash = {}
+
+    request_lines = request_lines.map do |line|
+      line.split(" ", 2)
+    end
+
+    request_lines = request_lines.map do |line|
+      request_lines_hash[line[0]] = line[1]
+    end
 
     request_format = ["Verb: #{verb}",
       "Path: #{path}",
       "Protocol: #{protocol}",
-      "Host: #{host}",
-      "Port: #{port}",
-      "Origin: #{host}",
-      "Accept: #{accept}"]
+      "Host: #{request_lines_hash["Host:"].split(":")[0]}",
+      "Port: #{request_lines_hash["Host:"].split(":")[1]}",
+      "Origin: #{request_lines_hash["Host:"].split(":")[0]}",
+      "Accept: #{request_lines_hash["Accept:"]}"]
     request_format
   end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -4,6 +4,12 @@ require 'socket'
 require 'hurley'
 class ServerTest < Minitest::Test
 
+  attr_reader :response
+
+  def setup
+    @response = Hurley.get("http://127.0.0.1:9292")
+    require "pry"; binding.pry
+  end
 
 
 
