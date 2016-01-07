3c9840cb00cc4c202034099d690bb011339341e5
Nick Weiland <nickweiland@gmail.com>
2016-01-06 21:09:26 -0700

    Refactor server class

--- lib/server.rb
+++ lib/server.rb
@@ -5,27 +5,24 @@ require 'request_formatter'
 require 'response'
 
 class Server
-  attr_reader :port
-
   def initialize(port = 9292)
     @port = port
     @server = TCPServer.new(port)
-    @total_requests = 0
-    @hello_world_count = 0
   end
 
   def request_response(server = @server)
+    total_requests = 0
+    hello_world_count = 0
     while (client = server.accept)
       request_object = Request.new(client, @port)
-      # request_format = RequestFormatter.new(request_object).request_format
-      Response.new(request_object, @total_requests, @hello_world_count)
-      @total_requests += 1
+      Response.new(request_object, total_requests, hello_world_count)
+      total_requests += 1
       if request_object.path == "/hello"
-        @hello_world_count += 1
+        hello_world_count += 1
       end
       break if request_object.path == "/shutdown"
     end
+    puts "Total requests: #{total_requests - 1}"
     client.close
-    puts "Total requests: #{@total_requests - 1}"
   end
 end
