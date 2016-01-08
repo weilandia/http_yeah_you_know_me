783a8daf0c85794b0c292f80ab268f1733eab050
Nick Weiland <nickweiland@gmail.com>
2016-01-07 23:12:55 -0700

    Response refactor

--- lib/response.rb
+++ lib/response.rb
@@ -54,8 +54,7 @@ class Response
     elsif hash["Path:"] == "/shutdown" then shut_it_down(total_requests)
     elsif hash["Path:"] == "/datetime" then datetime
     elsif hash["Path:"] == "/word_search" then word_search(request_object)
-    elsif hash["Path:"] == "/new_game"
-      new_game_message
+    elsif hash["Path:"] == "/new_game" then new_game_message
     elsif hash["Path:"] == "/start_game" then start_game && $game.start
     elsif hash["Path:"] == "/game" then $game.guess_tracker(guess(request_object)) && $game.game_path
     elsif hash["Path:"] == "/" then ""
