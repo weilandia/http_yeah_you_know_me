f8adda1aab1a8d3957c2c85ac6ff1909669b5722
Nick Weiland <nickweiland@gmail.com>
2016-01-07 20:56:12 -0700

    Refactor game

--- lib/game.rb
+++ lib/game.rb
@@ -26,30 +26,30 @@ class Game
     "<p>Total guesses: #{guess_counter}</p><p>Your last guess was #{last_guess} and #{high_low}"
   end
 
-  def win
-    "it was the right number! You Win!<p><form action='/start_game' method='post'>
-    <input type='Submit' value = 'Start New Game?'></input></form></p>"
-  end
-
   def guess_input_form
     "</p><form action='/game' method='post'>
     <input type='textarea' name='guess'></input>
     <input type='Submit'></input></form>"
   end
 
-  def too_low
+  def high_low
+    if last_guess == @answer then guess_win
+    elsif last_guess < @answer then guess_too_low
+    else guess_too_high end
+  end
+
+  def guess_win
+    "it was the right number! You Win!<p><form action='/start_game' method='post'>
+    <input type='Submit' value = 'Start New Game?'></input></form></p>"
+  end
+
+  def guess_too_low
     "it was too LOW.\nGuess again! #{
     guess_input_form}"
   end
 
-  def too_high
+  def guess_too_high
     "it was too HIGH.\nGuess again!#{
     guess_input_form}"
-  end
-
-  def high_low
-    if last_guess == @answer then win
-    elsif last_guess < @answer then too_low
-    else too_high end
   end
 end
